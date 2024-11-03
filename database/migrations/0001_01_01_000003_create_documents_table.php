5ee00c95d612b4026e93dfbc28a3f5f06ca40d4b
cebpereira <c.elandro.bp@gmail.com>
2024-11-03 19:37:51 -0300

    feat: adicionando migrations do Sanctum e de Documents

--- database/migrations/0001_01_01_000003_create_documents_table.php
+++ database/migrations/0001_01_01_000003_create_documents_table.php
@@ -14,8 +14,20 @@ return new class extends Migration
         Schema::create('documents', function (Blueprint $table) {
             $table->id('document_id');
             $table->uuid('user_id');
-            $table->string('title');
-            $table->text('content');
+            $table->string('user_name');
+            $table->string('user_role');
+            $table->string('user_document');
+            $table->string('product_brand');
+            $table->string('product_model');
+            $table->string('product_serial_number');
+            $table->string('product_processor');
+            $table->string('product_memory');
+            $table->string('product_disk');
+            $table->decimal('product_price', 10, 2);
+            $table->string('product_price_string');
+            $table->string('local');
+            $table->date('date');
+            $table->string('file_path')->nullable();
             $table->timestamps();
         });
 
